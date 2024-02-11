0bc701514fe94ca068baed40c3a132ea38fbac2e
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2024-02-11 16:17:37 +0530

    slot machine

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
 
-function App() {
-  return (
-    <div>
-    {/* <Heading/> */}
- 
-    </div>
+const SlotM = () => {
+ let x= '😄';
+ let y= '😄';
+ let z= '🧂';
+
+ if((x===y) && (y===z)){
+  return(
+    <>
+    {x} {y} {z}
+    <h3> This is matching</h3>
+    </>
   )
+ }else{
+  return(
+    <>
+    {x} {y} {z}
+    <h3> This is not  matching</h3>
+    </>
+  )
+ }
+}
+
+const App =() => {
+  return (
+    <>
+    <h1 style={{textAlign: 'center'}}> 🎰 Welcome to <span style={{fontWeight:"bold"}}>Slot Machine Game</span> 🎰</h1>
+    <SlotM/>
+    </>
+  );
 }
 
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './Card';
+// import Card from './Card';
 import './index.css';
-import Scard from './Cards';
+import App from './App';
+// import Scard from './Cards';
 
 
-function nvalue(val) {
-    console.log(val);
-    return(
-    <Card 
-    key={val.id}
-   image={val.image}
-   sname={val.sname}
-   Stitle={val.Stitle}
-   link={val.link}
-   />);
+// function nvalue(val) {
+//     console.log(val);
+//     return(
+//     <Card 
+//     key={val.id}
+//    image={val.image}
+//    sname={val.sname}
+//    Stitle={val.Stitle}
+//    link={val.link}
+//    />);
     
-}
+// }
 ReactDOM.render(
     <>
-    <h1 className='heading'>Top movies in Netflix</h1>
-   {Scard.map(nvalue)}
+    {/* <h1 className='heading'>Top movies in Netflix</h1>
+   {Scard.map(nvalue)} */}
 
-  
+   <App/>
     </>,
     document.getElementById('root')
 );
